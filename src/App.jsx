63ca1d182fbd015fade1f424955fb1e9c301538d
hassenzouzou <hassen.zouzou.acc@gmail.com>
2024-12-16 15:09:58 +0100

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Header from "./sections/Header";
+import "./index.css";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="overflow-hidden">
+      <Header />
+    </main>
+  );
 };
 
 export default App;
