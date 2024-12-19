037a6466060f19631d4371f8c0574eb341878518
hassenzouzou <hassen.zouzou.acc@gmail.com>
2024-12-19 21:08:25 +0100

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./sections/Header";
+import Hero from "./sections/Hero";
 import "./index.css";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
   );
 };
