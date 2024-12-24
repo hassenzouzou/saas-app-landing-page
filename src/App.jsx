3df1f541403f356e64eef9b9371254c40c9b25b9
hassenzouzou <hassen.zouzou.acc@gmail.com>
2024-12-24 20:54:30 +0100

    pricing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import "./index.css";
 import Features from "./sections/Features";
+import Pricing from "./sections/Pricing";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Hero />
       <Features />
+      <Pricing />
     </main>
   );
 };
